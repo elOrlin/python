e1ec408a34e16ef764edb373d73b2f4738d910af
Orlin de los santos <56376461+elOrlin@users.noreply.github.com>
2023-01-03 19:32:27 -0400

    parametros  opcionales

--- 08-funciones/main.py
+++ 08-funciones/main.py
@@ -21,3 +21,44 @@ def tabla(numero):
         print("\n")
         
 tabla(7)
+
+
+#parametros opcionales
+def getEmpleados(nombre, dni = None):
+    print("EMPLEADO")
+    print(f"nombre: {nombre}")
+    
+    if dni != None:
+        print(f"DNI: {dni}")
+    
+getEmpleados('Orlin', 123456789)
+
+def saludame(nombre):
+    saludo = f"Hola, saludos {nombre}"
+    
+    return saludo
+print(saludame('Orlin Diaz'))
+
+def calculadora(numero1, numero2, basicas = False):
+    
+    suma = numero1 + numero2
+    resta = numero1 - numero2
+    multi = numero1 * numero2
+    divicion = numero1 / numero2
+    
+    cadena = ""
+    
+    if basicas != False:
+        cadena += f"suma: {str(suma)}"
+        cadena = "\n"
+        cadena += f"resta: {str(resta)}"
+        cadena = "\n"
+    
+    else:
+        cadena += f"multi: {str(multi)}"
+        cadena = "\n"
+        cadena += f"divicion: {str(divicion)}"
+    
+        return cadena
+    
+print(calculadora(50, 5, True))
