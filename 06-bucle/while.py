db8e65900c1fca447d5ebaf9908697a800ad7326
Orlin de los santos <56376461+elOrlin@users.noreply.github.com>
2023-01-03 13:48:46 -0400

    creando while y haciendo un contador

---
+++ 06-bucle/while.py
@@ -0,0 +1,18 @@
+contador = 1
+
+while contador <= 100:
+    print(f"estoy en el numero {contador}")
+    contador = contador + 1
+    
+    
+    print("separado")
+    
+    
+contador = 1
+muestrame = str(0)
+
+while contador <= 100:
+    muestrame = muestrame + f", {str(contador)}"
+    contador = contador + 1
+    
+    print(muestrame)
