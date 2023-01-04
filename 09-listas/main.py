50b75d52527b943b8ab3bd48b1811f647c438fad
Orlin de los santos <56376461+elOrlin@users.noreply.github.com>
2023-01-03 21:40:46 -0400

    creando listas y tuplas

---
+++ 09-listas/main.py
@@ -0,0 +1,11 @@
+#crear listas en python
+
+peliculas = ['Batman', 'el rey de los anillos', 'terror de texas', 'the last kingdom']
+cantantes = list(('arcangel', 'daddy yankee', 'don omar'))
+years = list(range(2000, 2023))
+variada = ['Orlin', 30, 4, 76, True, 'Otniel']
+
+print(peliculas)
+print(cantantes)
+print(years)
+print(variada)
