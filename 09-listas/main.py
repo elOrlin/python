68fc0f604535b125f15cf4fb9ed6b52a0a0baead
Orlin de los santos <56376461+elOrlin@users.noreply.github.com>
2023-01-04 10:07:16 -0400

    recorrer las listas

--- 09-listas/main.py
+++ 09-listas/main.py
@@ -9,3 +9,51 @@ print(peliculas)
 print(cantantes)
 print(years)
 print(variada)
+
+#Indices 
+peliculas[1] = 'Gran Torino'
+print(peliculas[1])
+print(peliculas[-2])
+print(peliculas)
+
+print(cantantes[0:2])
+
+
+#recorrer las listas
+
+nueva_pelicula = ""
+
+while nueva_pelicula != 'parar':
+    nueva_pelicula = input('Introduce una nueva pelicula: ')
+    
+    if nueva_pelicula != 'parar':
+        peliculas.append(nueva_pelicula)
+        
+for pelicula in peliculas:
+    print(f"{peliculas.index(pelicula)+1}. {pelicula}")
+    
+    
+    #listas multidimencionales
+contactos = [
+    [
+        'orlin',
+        'orlindiaz@outlook.com'
+    ],  
+    [
+        'oliver',
+        'oliverdiaz@outlook.com'
+    ],
+    [
+        'olvin',
+        'olvindiaz@outlook.com'
+    ]
+]
+    
+    
+for contacto in contacto:
+    for elemento in contacto:
+        if contacto.index(elemento) == 0:
+            print(f"Nombre: {elemento}")
+            
+        else:
+            print(f"Apellidos: {elemento}")
