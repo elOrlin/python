d18745e5fdb4e6ea2678bc09c159e71eda7b5e59
Orlin de los santos <56376461+elOrlin@users.noreply.github.com>
2023-01-04 23:53:10 -0400

    modulos de matematicas

--- 12-modulos/main.py
+++ 12-modulos/main.py
@@ -20,3 +20,16 @@ print(fecha_completa)
 print(f"{fecha_completa.hour}:{fecha_completa.minute}")
 
 print(f"fecha personalizada {fecha_personalizada}")
+
+#Modulo matematicas
+import math
+
+print('Raiz cuadrada de 10:', math.sqrt(10))
+print('Numero pi:', float(math.pi))
+print('Redondear', math.ceil(6.56789))
+print('Redondear', math.floor(6.56789))
+
+#Modulo random
+import random
+
+print("Numero aleatorio entre 15 y 67:", random.randint(15, 67))
