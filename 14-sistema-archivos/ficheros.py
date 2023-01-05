a76cae37a6aa9280d64ad274ff0a53739829f948
Orlin de los santos <56376461+elOrlin@users.noreply.github.com>
2023-01-05 13:15:41 -0400

    escribiendo y cerrando ficheros de archivos

--- 14-sistema-archivos/ficheros.py
+++ 14-sistema-archivos/ficheros.py
@@ -1,4 +1,13 @@
 #Abrir ficheros
 from io import open
+import pathlib
 
-archivo = open("ficheros_texto.txt", "a+")
+ruta = str(pathlib.Path().absolute()) + "/ficheros_texto.txt"
+
+archivo = open(ruta, "a+")
+
+#escribir en un archivo
+archivo.write("************** SOY UN TEXTO DENTRO DE PYTHON ****************\n")
+
+#cerrar archivos
+archivo.close()
