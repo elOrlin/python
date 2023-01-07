5238c885dd0b6bead49742e838d86285b4b0f259
Orlin de los santos <56376461+elOrlin@users.noreply.github.com>
2023-01-07 12:37:05 -0400

    leer y copiar ficheros

--- 14-sistema-archivos/ficheros.py
+++ 14-sistema-archivos/ficheros.py
@@ -8,6 +8,27 @@ archivo = open(ruta, "a+")
 
 #escribir en un archivo
 archivo.write("************** SOY UN TEXTO DENTRO DE PYTHON ****************\n")
-
+print(archivo)
 #cerrar archivos
 archivo.close()
+
+#leer contenido
+ruta = str(pathlib.Path().absolute()) + "/14-sistema-archivos/ficheros_texto.txt"
+archivo_lectura = open(ruta, "r")
+
+contenido = archivo_lectura.read()
+for elemento in contenido:
+    print(f"este es el {elemento}")
+    
+#leer contenido y guardar lista
+lista = archivo_lectura.readlines()
+archivo_lectura.close()
+
+for frase in lista:
+    frase_lista = frase.split()
+    print(frase.center(100))
+    
+    #copiar archivo
+    ruta_original = str(pathlib.Path().absolute()) + "/14-sistema-archivos/ficheros_texto.txt"
+    ruta_nueva = str(pathlib.Path().absolute()) + "/14-sistema-archivos/ficheros_copiado.txt"
+    shutil.copyfile(ruta_original, ruta_nueva)
