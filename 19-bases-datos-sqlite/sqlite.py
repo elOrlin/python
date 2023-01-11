df14b44b93ccdf1856999f1661fe8bd2f3a6e423
Orlin de los santos <56376461+elOrlin@users.noreply.github.com>
2023-01-10 21:57:30 -0400

    subir de un golpe datos y actualizar

--- 19-bases-datos-sqlite/sqlite.py
+++ 19-bases-datos-sqlite/sqlite.py
@@ -22,17 +22,37 @@ conexion.commit()
 cursor.execute("INSERT INTO productos VALUES (null, 'Primer producto', 'Descriptcion de mi producto', 550)")
 conexion.commit()
 
-curosr.Execute("DELETE FROM productos")
+#eliminar productos
+#curosr.Execute("DELETE FROM productos")
+
+#insertar productos de un golpe
+productos = [
+    ('Ordenador portatil', 'Buen PC', 700),
+    ('Telefono Movil', 'Buen Telefono', 140),
+    ('Placa base', 'Buena Placa', 80),
+    ('Tablet 15', 'Buena Tablet', 300)
+]
+
+cursor.executemany("INSERT INTO PRODUCTOS VALUES (null,?,?,? )", productos)
+conexion.commit()
+
+#UPDATE
+cursor.execute("UPDATE productos SET precio=678 WHERE precio=80")
 
 #leer datos (listar datos)
-cursor.execute("SELECT * FROM productos")
+cursor.execute("SELECT * FROM productos WHERE precio >= 300;")
 productos = cursor.fetchall()
 
 for producto in productos:
-    print(f'titulo: {producto[1]}')
-    print(f'descripcion: {producto[2]}')
-    print(f'usuarios: {producto[3]}')
+    print(f'ID: {producto[0]}')
+    print(f'Titulo: {producto[1]}')
+    print(f'Descripcion: {producto[2]}')
+    print(f'Precio: {producto[3]}')
     print('\n')
+    
+cursor.execute("SELECT * FROM productos;")
+productos = cursor.fetchall()
+print(producto)
 
 #cerrar conexion
 conexion.close()
