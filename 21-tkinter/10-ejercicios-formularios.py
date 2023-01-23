e38c6f57ff84b817d0ebf00b83dd9c36f22272c4
Orlin de los santos <56376461+elOrlin@users.noreply.github.com>
2023-01-23 00:52:07 -0400

    creando calculadora

--- 21-tkinter/10-ejercicios-formularios.py
+++ 21-tkinter/10-ejercicios-formularios.py
@@ -5,7 +5,7 @@ ventana = Tk()
 
 ventana.geometry("300x300")
 
-ventana.title("Ejercicios completo con tkinter | Orlin Diaz")
+ventana.title("Calculadora | Orlin Diaz")
 ventana.config(
     bd=25
 )
@@ -14,17 +14,55 @@ numero1 = StringVar()
 numero2 = StringVar()
 resultado = StringVar()
 
-Label(ventana, text="Primer Numero: ").pack()
-Entry(ventana, textvariable=numero1).pack()
+marco = Frame(ventana, width=250, height=250)
+marco.config(
+    padx=15,
+    pady=15,
+    bd=5,
+    relief=SOLID
+)
 
-Label(ventana, text="Segundo Numero: ").pack()
-Entry(ventana, textvariable=numero2).pack()
+def convertirFloat(numero):
+    try:
+        result = float(numero)
+    except:
+        result = 0
+        messagebox.showerror("Error", "Introduce bien los datos")
+    return result
 
-Label(ventana, text="").pack()
+def sumar():
+    resultado.set(convertirFloat(numero1.get()) + convertirFloat(numero2.get()))
+    mostrarResultado()
+    
+def restar():
+    resultado.set(convertirFloat(numero1.get()) - convertirFloat(numero2.get()))
+    mostrarResultado()
+    
+def multiplicar():
+    resultado.set(convertirFloat(numero1.get()) * convertirFloat(numero2.get()))
+    mostrarResultado()
+    
+def dividir():
+    resultado.set(convertirFloat(numero1.get()) / convertirFloat(numero2.get()))
+    mostrarResultado()
+    
+def mostrarResultado():
+    messagebox.showinfo("Resultado", f"El resultado de la operacion es: {resultado.get()}")
+        
+marco.pack(side=TOP, anchor=CENTER)
+marco.pack_propagate(False)
 
-Button(ventana, text="Sumar").pack(side="left")
-Button(ventana, text="Restar").pack(side="left")
-Button(ventana, text="Multiplicar").pack(side="left")
-Button(ventana, text="Dividir").pack(side="left")
+Label(marco, text="Primer Numero: ").pack()
+Entry(marco, textvariable=numero1, justify="center").pack()
+
+Label(marco, text="Segundo Numero: ").pack()
+Entry(marco, textvariable=numero2, justify="center").pack()
+
+Label(marco, text="").pack()
+
+Button(marco, text="Sumar", command=sumar).pack(anchor=CENTER, fill=X, expand="YES")
+Button(marco, text="Restar", command=restar).pack(anchor=CENTER, fill=X, expand="YES")
+Button(marco, text="Multiplicar", command=multiplicar).pack(anchor=CENTER, fill=X, expand="YES")
+Button(marco, text="Dividir", command=dividir).pack(anchor=CENTER, fill=X, expand="YES")
 
 ventana.mainloop()
