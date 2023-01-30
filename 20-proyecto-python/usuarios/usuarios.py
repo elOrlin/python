f9bd219b6c88e815fb4fdc9a8e131d7873b3b6b3
Orlin de los santos <56376461+elOrlin@users.noreply.github.com>
2023-01-30 19:04:17 -0400

    manejo de excepciones y errores

--- 20-proyecto-python/usuarios/usuarios.py
+++ 20-proyecto-python/usuarios/usuarios.py
@@ -21,14 +21,18 @@ class Usuario:
         self.password = password
         
     def registrar(self):
-        fecha = '12/1/2023'
+        fecha = datetime.datetime.now()
         sql = "INSERT INTO usuarios VALUES(null, %s, %s, %s, %s, %s)"
         usuario = (self.nombre, self.apellidos, self.email, self.password, fecha)
         
-        cursor.execute(sql, usuario)
-        database.commit()
-        
-        return [cursor.rowcount, self]
+        try:
+            cursor.execute(sql, usuario)
+            database.commit()
+            result = [cursor.rowcount, self]
+        except:
+            result = [0, self]
+            
+        return result
         
     def identificar(self):
         return  self.nombre
